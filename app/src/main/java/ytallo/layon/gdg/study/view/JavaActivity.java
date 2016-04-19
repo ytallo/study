df014b5174d19370b01a0c035e2855ca2cd04b46
Ytallo <ytallo123@hotmail.com>
2016-04-18 22:59:37 -0300

    Ajuste na visualização das TabView e Inclusão da descrição do conteúdo de Tipo em Java

--- app/src/main/java/ytallo/layon/gdg/study/view/JavaActivity.java
+++ app/src/main/java/ytallo/layon/gdg/study/view/JavaActivity.java
@@ -6,36 +6,47 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TabHost;
 
-import ytallo.layon.gdg.study.MainActivity;
 import ytallo.layon.gdg.study.R;
 
 /**
  * Created by Ytallo on 16/04/2016.
  */
-public class JavaActivity extends DefualtActivity {
+public class JavaActivity extends AppCompatActivity {
+
+    private TabHost host;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.onDefaultCreate(savedInstanceState);
 
-        TabHost host = (TabHost) findViewById(R.id.tabHost);
+        setContentView(R.layout.java_activity);
+
+        host = (TabHost) findViewById(R.id.tabHost);
         host.setup();
+        addTab(R.id.tab1, "Tipos");
+        addTab(R.id.tab2, "E/S");
+        addTab(R.id.tab3, "Arrays");
+        addTab(R.id.tab4, "Estrut. Decisão");
+        addTab(R.id.tab5, "Loops");
+    }
 
-        TabHost.TabSpec spec = host.newTabSpec("Tipos");
-        spec.setContent(R.id.tab1);
-        spec.setIndicator("Tipos");
+    private void addTab(int layout, String name) {
+
+        TabHost.TabSpec spec = host.newTabSpec(name);
+        spec.setContent(layout);
+        spec.setIndicator(name);
         host.addTab(spec);
     }
 
-    @Override
-    public int getToolbarResourceId() {
-        return R.id.toolbar;
-    }
 
-    @Override
-    public int getLayoutResourceId() {
-        return R.layout.java_activity;
-    }
+//    @Override
+//    public int getToolbarResourceId() {
+//        return R.id.toolbar;
+//    }
+//
+//    @Override
+//    public int getLayoutResourceId() {
+//        return R.layout.java_activity;
+//    }
 
 }
